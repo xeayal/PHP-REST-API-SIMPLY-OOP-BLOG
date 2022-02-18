f93ce2fe8e0a83fb3e54a6c5119efda340cecbbd
Khayal Abilhasanov <44834300+xeayal@users.noreply.github.com>
2022-02-18 19:44:05 +0300

    Update login.model.php

--- app/model/login.model.php
+++ app/model/login.model.php
@@ -1,7 +1,7 @@
 <?php
 class Auth extends Controller {
     public static function notSession(){
-        header('location:http://localhost/metakMVC/login');
+        header('location:' . PATH . 'login');
     }
     public function login($password){
         $sql = 'SELECT *FROM users WHERE password = ?';
@@ -10,7 +10,10 @@ class Auth extends Controller {
         $user = $user->fetch();
         if($user){
            $_SESSION['userlogin'] = $user['id'];
-           header('location:http://localhost/metakMVC/admin');
+           header('location:' . PATH . 'admin');
+        }
+        else{
+            echo '<script>alert("ŞİFRƏ YANLIŞDIR")</script>';
         }
     }
 }
