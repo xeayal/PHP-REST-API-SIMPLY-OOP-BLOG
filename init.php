7838bc97599daecb5e05e96b1f7de4cf0cd76567
Khayal Abilhasanov <44834300+xeayal@users.noreply.github.com>
2022-02-18 11:40:31 +0300

    Delete init.php

--- init.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'app/core/connect.php';
-require_once 'app/core/router.php';
-require_once 'app/core/controller.php';
-require_once 'app/core/default.php';
-require_once 'app/model/login.model.php';
-
