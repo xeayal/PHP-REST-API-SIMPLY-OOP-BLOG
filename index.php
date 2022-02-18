aa2889e9ba4732f4012e7e560ad22eff49ca0908
Khayal Abilhasanov <44834300+xeayal@users.noreply.github.com>
2022-02-18 11:39:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+ob_start();
+
+define("PATH","http://localhost/metakMVC/");
+
+require_once __DIR__. '/app/init.php';
+
+$url = trim($_GET['url']);
+
+if($url == ''){
+    $url = 'home';
+}
+
+new Router($url);
+
+Router::get("home",    "home/index");
+Router::get("login",   "login/index");
+Router::get("logout",  "logout/index");
+
+Router::get("admin", function(){
+    global $url;
+    $page = explode('/', $url)[1];
+    if($page == 'addpost'){
+        require_once 'app/controller/addpost.php';
+        $tt = new $page();
+        $tt -> index();
+    }
+    elseif($page == 'posts' || $page == ''){
+        require_once 'app/controller/admin.php';
+        $tt = new Admin();
+        $tt -> index();
+    }
+
+
+});
+
+Router::get('posts', function(){
+    global $url;
+    $pagination  = explode('/', $url)[1];
+    require_once 'app/controller/posts.php';
+    $class = new posts();
+    $class -> index($pagination);
+});
+
+Router::get('post', function(){
+    global $url;
+    $url  = explode('/', $url)[1];
+    require_once 'app/controller/post.php';
+    $class = new post();
+    $class -> index($url);
+});
