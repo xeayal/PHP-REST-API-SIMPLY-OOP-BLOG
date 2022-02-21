3e0b8519c118bc6560bf9741a6b4774bcd78157e
Khayal Abilhasanov <44834300+xeayal@users.noreply.github.com>
2022-02-21 18:23:49 +0300

    Update addpost.view.php

--- app/view/admin/addpost.view.php
+++ app/view/admin/addpost.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POST ƏLAVƏ ET</title>
+    <title>POSTLAR</title>
     <link rel="stylesheet" href="<?=PATH ?>app/view/css/bootstrap.min.css">
 </head>
 <body>
@@ -28,10 +28,10 @@
                     <h3 class="text-center mb-5">YENİ POST ƏLAVƏ ET</h3>
                     <form action="" method="post" enctype="multipart/form-data">
                         <div class="form-group">
-                            <input type="text" class="form-control" name="name" placeholder="POST BAŞLIĞI...">
+                            <input type="text" class="form-control" name="name" placeholder="POST BAŞLIĞI..." required>
                         </div>
                         <div class="form-group">
-                            <textarea class="form-control" name="text" name="text" rows="5" placeholder="POST TEXTİ..."></textarea>
+                            <textarea class="form-control" name="text" name="text" rows="5" placeholder="POST TEXTİ..." required></textarea>
                         </div>
                         <div class="custom-file mb-3">
                             <input type="file" class="custom-file-input" name="img" required>
